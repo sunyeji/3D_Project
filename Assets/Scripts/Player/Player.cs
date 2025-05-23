a09af21bb932c7028cff3854cc217346666e1b37
sunyeji <ssuu0167@gmail.com>
2025-05-23 14:12:37 +0900

    adit : 코드 수정
    점프패드 높이 조절

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -8,15 +8,15 @@ public class Player : MonoBehaviour
     public PlayerController controller;
     private void Awake() 
     {
-        PlayerManager.Instance.Player = this;  
-        controller = GetComponent<PlayerController>();
+        PlayerManager.Instance.Player = this;             // 플레이어 매니저에 현재 플레이어 인스턴스를 등록
+        controller = GetComponent<PlayerController>();    // 이 오브젝트에서 PlayerController 컴포넌트를 가져옴
     }
-    
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("JumpPad"))
+        if (other.CompareTag("JumpPad"))                  // 충돌한 객체의 태그가 "JumpPad"이면
         {
-            controller.JumpByPad(10f); // 자동 점프
+            controller.JumpByPad(5f);                    // 점프 패드를 통해 위로 점프 (점프력 5f)
         }
     }
     
